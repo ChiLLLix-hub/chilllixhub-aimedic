9d0fa2e4a29120d1ec9d9a2277b4f7bcaddb6eea
ChiLLLix-hub <chilllix.me@gmail.com>
2025-12-16 16:17:43 +0800

    Update hospital bed locations in config.lua

--- config.lua
+++ config.lua
@@ -17,13 +17,13 @@ Config.TreatmentTimePerPlayer = 10000 -- Time to treat each player (ms)
 
 -- Hospital bed locations for patient teleport after revival
 Config.HospitalBeds = {
-    {coords = vector3(356.73, -585.27, 43.31), heading = 252.0}, -- Pillbox Hill Medical Center
-    {coords = vector3(353.14, -584.55, 43.31), heading = 252.0},
-    {coords = vector3(349.76, -583.81, 43.31), heading = 252.0},
-    {coords = vector3(346.19, -583.04, 43.31), heading = 252.0},
-    {coords = vector3(335.70, -580.13, 43.31), heading = 252.0},
-    {coords = vector3(332.21, -579.34, 43.31), heading = 252.0},
-    {coords = vector3(328.61, -578.54, 43.31), heading = 252.0},
-    {coords = vector3(325.06, -577.76, 43.31), heading = 252.0},
+    {coords = vector3(357.38, -594.42, 42.88), heading = 340.3}, -- Pillbox Hill Medical Center
+    {coords = vector3(354.23, -592.68, 42.87), heading = 340.0},
+    {coords = vector3(350.72, -591.76, 42.87), heading = 340.0},
+    {coords = vector3(346.91, -590.64, 42.87), heading = 340.0},
+    {coords = vector3(360.42, -587.03, 42.87), heading = 160.0},
+    {coords = vector3(356.74, -585.94, 42.87), heading = 160.0},
+    {coords = vector3(353.24, -584.69, 42.87), heading = 160.0},
+    {coords = vector3(349.69, -583.61, 42.87), heading = 160.0}
 }
 
